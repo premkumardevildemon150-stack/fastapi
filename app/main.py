6f32449bb4137d508eb4ca9ef2d2c41abdcdd082
premkumardevildemon150-stack <premkumardevildemon150@gmail.com>
2026-04-30 09:21:05 +0530

    first commit

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.routes import user
+
+app = FastAPI()
+
+app.include_router(user.router)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to FastAPI Project"}
